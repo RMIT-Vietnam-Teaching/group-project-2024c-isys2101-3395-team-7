74c715053701ae5885526270ae1542235ba40f2d
Doanh Tran <s3927023@rmit.edu.vn>
2024-11-23 12:36:47 +0700

    implemented UI for User Profile dropdown

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,13 @@
 'use client'
 import React, {useState} from "react"
 import Link from "next/link";
+import Favorite from "@/components/Favorite";
 
 function Header () {
     const [isOpenHistory, setIsOpenHistory] = useState(false);
+    const [isOpenFavorite, setIsOpenFavorite] = useState(false);
+    const [isOpenProfile, setIsOpenProfile] = useState(false);
+    const [isOpenSetting, setIsOpenSetting] = useState(false);
 
 
     // TODO: fix history toggle open bug and favorite toggle
@@ -31,15 +35,63 @@ function Header () {
                             </button>
                             {/* Conditionally render History */}
                             {isOpenHistory && <History />}
-                            <button className="mx-5">
+                            <button
+                                onClick={() => setIsOpenFavorite(!isOpenFavorite)}
+                                className="mx-5">
                                 <img src={'Bookmark_black.svg'} alt="Favorite Icon" className={'h-10 w-10'}/>
                             </button>
-                            <button className="mx-5">
+                            {/* Conditionally render Favorite */}
+                            {isOpenFavorite && <Favorite />}
+                            <button
+                                onClick={() => setIsOpenSetting(!isOpenSetting)}
+                                className="mx-5">
                                 <img src={'Setting_fill.svg'} alt="Setting Icon" className={'h-10 w-10'}/>
                             </button>
-                            <button className="mx-2">
+                            {/*Drop down to show current setting*/}
+                            {isOpenSetting && (
+                                <div
+                                    className="absolute top-12 right-0 mt-2 w-48 bg-white shadow-lg rounded-lg border border-gray-200"
+                                >
+                                    <ul className="py-1">
+                                        <li>
+                                            consideration for toggl switch to change language
+                                        </li>
+                                        <li>
+                                            consideration for voice speed
+                                        </li>
+                                    </ul>
+                                </div>
+                            )}
+                            <button
+                                onClick={() => setIsOpenProfile(!isOpenProfile)}
+                                className="mx-2">
                                 <img src={'User_circle.svg'} alt="User" className={'h-10 w-10'}/>
                             </button>
+                            {/* Dropdown Menu */}
+                            {isOpenProfile && (
+                                <div
+                                    className="absolute top-12 right-0 mt-2 w-48 bg-white shadow-lg rounded-lg border border-gray-200"
+                                >
+                                    <ul className="py-1">
+                                        <li>
+                                            <button
+                                                className="block px-4 py-2 text-gray-700 hover:bg-gray-100 w-full text-left"
+                                                onClick={() => console.log("Profile Clicked")}
+                                            >
+                                                View Profile
+                                            </button>
+                                        </li>
+                                        <li>
+                                            <button
+                                                className="block px-4 py-2 text-red-600 hover:bg-gray-100 w-full text-left"
+                                                onClick={() => console.log("Logout Clicked")}
+                                            >
+                                                Logout
+                                            </button>
+                                        </li>
+                                    </ul>
+                                </div>
+                            )}
                         </div>
                     </div>
             </div>
