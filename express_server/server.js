ac3d89afe2c9355649b4d000a71f0e86bf77f9dc
Hiep Hoang <85355255+HiepHoang132@users.noreply.github.com>
2024-11-27 00:09:38 +0700

    Fix import path for record router

--- express_server/server.js
+++ express_server/server.js
@@ -19,7 +19,7 @@ db.on("error", (error) => console.error(error));
 
 db.once("open", () => console.log("Connected to database"));
 
-const recordRouter = require("./controllers/record");
+const recordRouter = require("./routes/record");
 app.use("/record", recordRouter);
 
 app.listen(PORT, () => {
