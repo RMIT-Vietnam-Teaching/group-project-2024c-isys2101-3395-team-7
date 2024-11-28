ea3ba9f96581b980174064ff0c680cce9c5dfa15
IamLucifer0312 <117656374+IamLucifer0312@users.noreply.github.com>
2024-11-28 23:24:16 +0700

    Revert "remove CORS again"
    This reverts commit 113ff6b78af52ddad350fba9cf738b7c900a01fb.

--- openai_server/image_extraction.py
+++ openai_server/image_extraction.py
@@ -1,4 +1,5 @@
 import os
+import openai
 import base64
 from flask import Flask, request, jsonify
 from openai import OpenAI
@@ -11,6 +12,12 @@ load_dotenv(override=True)
 register_heif_opener()
 
 app = Flask(__name__)
+CORS(app)
+openai.proxies = {
+    "http": os.getenv("HTTP_PROXY"),
+    "https": os.getenv("HTTPS_PROXY")
+}
+
 # Load the API key from an environment variable
 api_key = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=api_key)
