d08b2e52c74573e8a4655492035df94498fdeac9
IamLucifer0312 <117656374+IamLucifer0312@users.noreply.github.com>
2024-11-29 00:55:49 +0700

    Change openai version (would fix the issue)

--- openai_server/image_extraction.py
+++ openai_server/image_extraction.py
@@ -1,20 +1,21 @@
 import os
-import openai
 import base64
 from flask import Flask, request, jsonify
 from openai import OpenAI
 from dotenv import load_dotenv
 from PIL import Image
 from pillow_heif import register_heif_opener
+from flask_cors import CORS
 
 load_dotenv(override=True)
 register_heif_opener()
 
 app = Flask(__name__)
+CORS(app)
 
 # Load the API key from an environment variable
 api_key = os.getenv("OPENAI_API_KEY")
-client = OpenAI(api_key=api_key)
+client = OpenAI(api_key=api_key) 
 MODEL = "gpt-4o"
 
 def encode_image(image_path):
