a67a4ca2ff26e2bcc8e1e591c75f2a0521d78cb7
Doanh Tran <s3927023@rmit.edu.vn>
2024-11-29 17:35:42 +0700

    implemented /config -page

---
+++ client/src/app/config-page/page.jsx
@@ -0,0 +1,11 @@
+import ConfigPage from "@/components/config-page";
+
+const configpage = () => {
+    return (
+        <>
+            <ConfigPage/>
+        </>
+    )
+}
+
+export default configpage
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -20,6 +20,23 @@ module.exports = {
       colors: {
         // background: "var(--background)",
         // foreground: "var(--foreground)",
+
+        primary: '#FFD369',
+        lightPrimary: '#ffe8b3',
+        darkPrimary: '#e6a100',
+
+        danger: '#E01B00',
+        darkDanger: '#BE1700',
+        lightDanger: '#FCE8E5',
+
+        success: '#009D4F',
+        lightSuccess: '#E5F5ED',
+        darkSuccess: '',
+
+        warning: '#FFB600',
+        lightWarning: '#FFF8E5',
+        darkWarning: ''
+
       },
     },
   },
