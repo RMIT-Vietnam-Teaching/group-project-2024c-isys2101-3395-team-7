92a3c336c869ff12f920b7212fee18b00294cb4c
IamLucifer0312 <117656374+IamLucifer0312@users.noreply.github.com>
2024-12-15 18:49:42 +0700

    config: change message

--- express_server/routes/userRoute.js
+++ express_server/routes/userRoute.js
@@ -61,13 +61,13 @@ router.post("/login", upload.none(), async (req, res) => {
     const user = await User.findOne({ username });
 
     if (!user) {
-      return res.status(401).json({ message: "Invalid username or password" });
+      return res.status(401).json({ message: "Invalid username" });
     }
 
     // Compare the password
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(401).json({ message: "Invalid username or password" });
+      return res.status(401).json({ message: "Invalid password" });
     }
 
     // Generate JWT token
