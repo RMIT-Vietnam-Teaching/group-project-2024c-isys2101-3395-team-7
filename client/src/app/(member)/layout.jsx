cb2a77b761199b090b095cbe670c95a83fadcfe1
NhanNguyen20 <nguyenlethunhan@gmail.com>
2024-12-30 17:23:26 +0700

    fix error for deploy

--- client/src/app/(member)/layout.jsx
+++ client/src/app/(member)/layout.jsx
@@ -1,25 +1,28 @@
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import { Suspense } from "react";
 
 const layout = ({ children }) => {
     return (
         <>
-            <div>
-                <div className={'flex flex-col h-[100vh]'}>
-                    <div className={'h-auto'}>
-                        <Header />
-                    </div>
-                    <div className='flex flex-row w-[100%] overflow-hidden' style={{ minHeight: "calc(100vh - 50px)" }}>
-                        <NavBar />
-                        <div className='flex-1 overflow-auto'>
-                            {children}
+            <Suspense>
+                <div>
+                    <div className={'flex flex-col h-[100vh]'}>
+                        <div className={'h-auto'}>
+                            <Header />
                         </div>
-                        {/* <Footer /> */}
-                    </div>
+                        <div className='flex flex-row w-[100%] overflow-hidden' style={{ minHeight: "calc(100vh - 50px)" }}>
+                            <NavBar />
+                            <div className='flex-1 overflow-auto'>
+                                {children}
+                            </div>
+                            {/* <Footer /> */}
+                        </div>
 
+                    </div>
                 </div>
-            </div>
+            </Suspense>
         </>
     )
 }
