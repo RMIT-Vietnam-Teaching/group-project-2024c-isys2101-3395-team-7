5b5f8e40a7c578f315fbec0d56a7fbb44c5a8e25
Doanh Tran <s3927023@rmit.edu.vn>
2025-01-06 16:12:21 +0700

    Updated guidelines page's content (unfinished)

--- client/src/components/guidelines/index.jsx
+++ client/src/components/guidelines/index.jsx
@@ -36,17 +36,15 @@ export default function guidelines() {
                         </p>
                         <ul>
                             <li>
-                                Recognition: The system will attempt to recognize the handwritten text and display it in
+                                <strong>Recognition</strong>: The system will attempt to recognize the handwritten text and display it in
                                 digital format.
                             </li>
                             <li>
-                                Correction: The system will identify and suggest corrections for any misrecognized
+                                <strong>Correction</strong>: The system will identify and suggest corrections for any mis-recognized
                                 characters
                             </li>
-
                             <li>
-
-                                Review & Learning: Users can review the system's analysis, learn from the corrections,
+                                <strong>Review & Learning</strong>: Users can review the system's analysis, learn from the corrections,
                                 and resubmit their handwriting for further refinement.
 
                             </li>
@@ -59,9 +57,6 @@ export default function guidelines() {
                             height={873}
                             alt="Handwriting Main Screen"
                         />
-                        <p>
-
-                        </p>
                     </div>
                     <div className={"grid grid-cols-2 gap-4"}>
                         <div>
@@ -69,7 +64,21 @@ export default function guidelines() {
                                 STEP 1:
                             </p>
                             <p>
-                                Upload your file with hand-writing in Vietnamese
+                                <strong>Browse your file</strong>: Choose the image containing your handwritten Vietnamese text. Supported file formats include:
+                                <ul>
+                                    <li>
+                                        JPEG (.jpg, .jpeg)
+                                    </li>
+                                    <li>
+                                        PNG (.png)
+                                    </li>
+                                    <li>
+                                        PDF (.HEIC)
+                                    </li>
+                                </ul>
+                                    <strong>Choose another file</strong>: Choose another image.
+                                <br/>
+                                    <strong>Submit file</strong>: Click the button to send the Image to our Service.
                             </p>
                         </div>
                         <div className={"grid justify-items-center"}>
@@ -83,11 +92,12 @@ export default function guidelines() {
                     </div>
                     <div className={"grid grid-cols-2 gap-4"}>
                         <div>
-                            <p className="font-semibold text-pink">
+                            <strong className="font-semibold text-pink">
                                 STEP 2:
-                            </p>
+                            </strong>
                             <p>
-                                The system will be processing your prompt
+                                The system will begin processing your handwriting.
+                                This may take a few seconds or minutes depending on the complexity and size of the file.
                             </p>
                         </div>
                         <div className={"grid justify-items-center"}>
@@ -100,13 +110,14 @@ export default function guidelines() {
                         </div>
                     </div>
                     <p className="font-semibold text-pink">
-                        STEP 3:
+                        STEP 3: Once processing is complete, the system will display:
                     </p>
                     <Image src={"/handwriting-feedback.png"}
                            width={1776}
                            height={873}
                            alt={"Handwriting Feedback"}
                     />
+
                     <div className={"grid grid-cols-2 gap-4 justify-items-center"}>
                         <div className={""}>
                             <Image
@@ -118,16 +129,28 @@ export default function guidelines() {
                         </div>
                         <div>
                             <p>
-                                The system will return the corrected version and point out the incorrect words, letters,
-                                dialects in wrong handwriting form.
+                                On the left side: The system will display your uploaded file for better comparison.
+                                <ul>
+                                    <li>
+                                        <strong>Uploaded file</strong>: You submitted request will display here.
+                                    </li>
+                                    <li>
+                                        <strong>Upload a new One</strong>: Upload the same handwriting file again for another attempt or Upload a different handwriting file for further analysis.
+                                    </li>
+                                </ul>
+
                             </p>
                         </div>
                     </div>
                     <div className={"grid grid-cols-2 gap-4 justify-items-center"}>
                         <div>
                             <p>
-                                The system will return the corrected version and point out the incorrect words, letters,
-                                dialects in wrong handwriting form.
+                                On the right side: The system will return the corrected version and point out the incorrect words, letters,
+                                dialects in wrong handwriting form:
+                                <ul>
+                                    <li><strong>Feedback</strong>: Any incorrect grammars, spellings, or vocabularies will be noted and display here, alongside with suggestions, recommendations for improvements.</li>
+                                    <li><strong>Recognized Text</strong>: The system's transcript of your handwriting.</li>
+                                </ul>
                             </p>
                         </div>
                         <div className={""}>
@@ -143,7 +166,23 @@ export default function guidelines() {
                 {/*Voice Section*/}
                 <CollapsibleSection title="Use Voice Detection and Correction Feature">
                     <p>
-                        The Voice Detection and Correction Feature will...
+                        This feature allows users to upload recorded Vietnamese speech and receive instant
+                        feedback, including:
+                        <ul>
+                            <li>
+                                <strong>Recognition</strong>: The system will attempt to recognize the recorded speech and display it in
+                                digital format.
+                            </li>
+                            <li>
+                                <strong>Correction</strong>: The system will identify and suggest corrections for any mis-recognized
+                                characters.
+                            </li>
+                            <li>
+                                <strong>Review & Learning</strong>: Users can review the system's analysis, learn from the corrections,
+                                and resubmit their records for further suggestions.
+
+                            </li>
+                        </ul>
                     </p>
                     <Image
                         src="/voice-main-screen.png"
@@ -156,9 +195,26 @@ export default function guidelines() {
                             <p className="font-semibold text-pink">
                                 STEP 1:
                             </p>
-                            <p>
-                                Upload your file with hand-writing in Vietnamese
-                            </p>
+                            <div>
+                                <p>
+
+                                    Before Recording: This area initially displays instructions on how to achieve optimal audio quality for recording. These instructions might include:
+                                    <strong>Browse your file</strong>: Choose the recording containing your handwritten
+                                    Vietnamese text. All audio file formats are currently supported.
+                                    <br/>
+                                    <strong>Or</strong>,
+                                    <br/>
+                                    Press <strong>Start</strong>: to record an audio on the spot. It is recommended that you are using a quiet environment with minimal background noise, speaking clearly and at a moderate pace, and ensuring proper microphone placement.
+                                    <br/>
+                                    Press <strong>Stop</strong>: to stop and conclude the recording.
+                                    <br/>
+                                    Press <strong>Start</strong> again: to start a brand new recording.
+                                    <br/>
+                                    <strong>Choose another file</strong>: Choose another audio file.
+                                    <br/>
+                                    <strong>Submit file</strong>: Click the button to send the Recording to our Service.
+                                </p>
+                            </div>
                         </div>
                         <div className={"grid justify-items-center"}>
                             <Image
@@ -175,7 +231,8 @@ export default function guidelines() {
                                 STEP 2:
                             </p>
                             <p>
-                                Loading
+                                The system will begin processing your audio.
+                                This may take a few seconds or minutes depending on the complexity and size of the file.
                             </p>
                         </div>
                         <div className={"grid justify-items-center"}>
@@ -210,15 +267,29 @@ export default function guidelines() {
                             </div>
                             <div>
                                 <p>
-                                    feedback
+                                    Left Section: Recorded Audio and Playback:
+                                    <ul>
+                                        <li><strong>Original Audio</strong>: This section will display a waveform visualization of the recorded audio. You can replay the audio</li>
+                                        <li><strong>Transcribed Audio</strong>: This area will transcribe what you spoke in the file above.</li>
+                                    </ul>
                                 </p>
                             </div>
                         </div>
                         <div className={"grid grid-cols-2 gap-4"}>
                             <div>
-
                                 <p>
-                                    feedback
+                                    Right Section: Recording Controls and Feedback
+                                    <ul>
+                                        <li><strong>Corrected audio version with AI voice</strong>: This section will display a waveform visualization of the audio spoken by an AI voice. You can play this audio to listen to the corrected version spoken in corrected tone by the AI.</li>
+                                        <li><strong>Feedback</strong>: Detailed feedback on pronunciation, grammar, and vocabulary. This might include:
+                                            <ul>
+                                                <li>Identifying specific sounds that were mispronounced.</li>
+                                                <li>Providing examples of correct pronunciation.</li>
+                                                <li>Offering suggestions for improving fluency and intonation.</li>
+                                            </ul>
+                                            </li>
+                                        <li><strong>Audio Transcript:</strong>: The system's transcript of your Original Audio.</li>
+                                    </ul>
                                 </p>
                             </div>
                             <div className={"grid justify-items-center"}>
@@ -236,7 +307,13 @@ export default function guidelines() {
                 {/*user activity section*/}
                 <CollapsibleSection title="Use Activity Record Feature">
                     <p>
-                        To re-view your history and favorite items storage, visit....
+                        This feature allows you to manage your history and favorite sections. You can:
+                        <ul>
+                            <li>Switch between History and Favorite Tab.</li>
+                            <li>Filter: by Voice or Handwriting Feature.</li>
+                            <li>Add the item to favorite, the item will also appear in Favorite Tab.</li>
+                            <li>Delete the item permanently from both lists.</li>
+                        </ul>
                     </p>
                     <Image
                         src="/user-activity-main-screen.png"
@@ -254,9 +331,8 @@ export default function guidelines() {
                             />
                         </div>
                         <div>
-
                             <p>
-                                pop up
+                                Clicking on the item will review the detail.
                             </p>
                         </div>
                     </div>
@@ -293,8 +369,7 @@ export default function guidelines() {
                 {/*Exercises Section*/}
                 <CollapsibleSection title="Use Exercises Feature">
                     <p>
-                        The Sentence Correction Tool analyzes your sentences and provides
-                        grammar suggestions to improve writing skills.
+                        This feature allows you to practice your existing knowledge by passing exercises.
                     </p>
                     <Image
                         src="/exercises-main-screen.png"
